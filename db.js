8c90a67920186330f5586b7b297088b20a1cbaa1
Gaurav <myselfgroot@gmail.com>
2021-05-28 21:32:16 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,40 @@
+{
+  "users": [
+    {
+      "id": 1,
+      "name": "Gaurav",
+      "courses": [
+        {
+          "id": "c-1",
+          "name": "java"
+        },
+        {
+          "id": "c-2",
+          "name": "sql"
+        },
+        {
+          "id": "c-1",
+          "name": "php"
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "name": "Saurav",
+      "courses": [
+        {
+          "id": "c-4",
+          "name": "html"
+        },
+        {
+          "id": "c-5",
+          "name": "css"
+        },
+        {
+          "id": "c-6",
+          "name": "javascript"
+        }
+      ]
+    }
+  ]
+}
